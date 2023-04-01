a5a182d8cb05557149acae8a5cfa5fdf496e23cd
Samarth Gupta <samarthgupta1202@gmail.com>
2023-04-01 14:03:20 +0530

    fonts added

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,20 +11,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage("images/profile.jpg"),
+            backgroundColor: Colors.teal,
+            body: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: AssetImage("images/profile.jpg"),
+                  ),
+                  Text(
+                    "Samarth",
+                    style: TextStyle(
+                        fontSize: 40.0,
+                        fontFamily: "Pacifico-Regular",
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "FLUTTER DEVELOPER",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontFamily: "Source Sans Pro",
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5,
+                      color: Colors.teal[100],
+                    ),
+                  )
+                ],
               ),
-            ],
-          ),
-        ]),
-      ),
-    ));
+            )));
   }
 }
